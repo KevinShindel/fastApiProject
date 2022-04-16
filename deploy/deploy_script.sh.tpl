f1e27035ef7c591739c43ec90bbb0221aacacd83
KevinShindel <kevin.shindel@yahoo.com>
2022-04-16 22:24:45 +0300

    Add deploy script

---
+++ deploy/deploy_script.sh.tpl
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install nginx git virtualenv -y
+
+cd /opt
+mkdir ${project_name}
+
+
+echo "server {
+        listen 80;
+        server_name ${project_name};
+        location /static {
+          alias /opt/${project_name}/static;
+        }
+        location / {
+                proxy_set_header Host $host;
+                proxy_pass http://unix:/tmp/${project_name}.socket;
+        }
+}" > ${project_name}.config
+
+sudo mv ${project_name}.config /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/${project_name}.config /etc/nginx/sites-enabled/${project_name}.config
+
+sudo git clone https://${gh_token}@github.com/KevinShindel/fastApiProject ${project_name}
+sudo virtualenv -p python venv
+cd ${project_name}
+. ../venv/bin/activate
+pip install -r requirements.txt
+
+# launch server
+python -m uvicorn main:app --udx=/tmp/${project_name}.socket # run on socket
---
+++ deploy/main.tf
@@ -0,0 +1,80 @@
+#------------------------------
+# Task:
+# 
+# Created by Kevin Shindel
+#------------------------------
+
+variable "gh_token" {
+  type = string
+  description = "GitHub token"
+}
+
+variable "project_name" {
+  type = string
+  description = "ProjectName"
+}
+
+variable "public_key" {
+  type = string
+  description = "ssh public key"
+}
+
+provider "aws" {
+  profile = "default"
+}
+
+data "aws_ami" "latest_ubuntu" {
+  owners = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_security_group" "this" {
+  name = "Military Server"
+
+  dynamic "ingress" {
+    for_each = ["80", "443", "22"]
+    content {
+      to_port = ingress.value
+      from_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "this" {
+  instance = aws_instance.server.id
+}
+
+resource "aws_instance" "server" {
+  key_name = aws_key_pair.this.id
+  vpc_security_group_ids  = [aws_security_group.this.id]
+  ami                     = data.aws_ami.latest_ubuntu.id
+  instance_type           = "t2.micro"
+
+  user_data               = templatefile("deploy_script.sh.tpl", {
+    project_name = var.project_name
+    gh_token = var.gh_token
+  })
+
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = "aws_key"
+  public_key = file(var.public_key)
+}
+
+output "instance_public_url" {
+  value = aws_eip.this.public_dns
+}
